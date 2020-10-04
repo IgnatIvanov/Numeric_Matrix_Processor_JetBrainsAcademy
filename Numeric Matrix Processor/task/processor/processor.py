3d8c6d4029ae060d936a9b7c3d2da1d991192b98
IgnatIvanov <ignativanov1996@mail.ru>
2020-10-04 21:40:45 +0300

    2020.10.04.2140 Stage 1 complete.

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,1 +1,18 @@
-print('Hello, world!')
+answer = []
+
+dimmensions1 = input().split(' ')
+matrix1 = []
+for row in range(int(dimmensions1[0])):
+    matrix1.append([int(x) for x in input().split(' ')])
+
+dimmensions2 = input().split(' ')
+matrix2 = []
+for row in range(int(dimmensions2[0])):
+    matrix2.append([int(x) for x in input().split(' ')])
+
+if dimmensions2 != dimmensions1:
+    print('ERROR')
+else:
+    for row in range(int(dimmensions1[0])):
+        answer.append([matrix1[row][column] + matrix2[row][column] for column in range(int(dimmensions1[1]))])
+        print(*answer[row])
